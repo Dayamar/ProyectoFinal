e8afdab98cd8357cedd7cf3a40f841a85f7a8ed0
Dayaneth Arrocha <dayamar0409@gmail.com>
2020-04-26 10:46:05 -0500

    Mensaje para exponer

--- src/proyectoPrueba/ProyPrueba.java
+++ src/proyectoPrueba/ProyPrueba.java
@@ -3,7 +3,7 @@ package proyectoPrueba;
 public class ProyPrueba {
 
 	public static void main(String[] args) {
-		System.out.println("Practicando editar mensajes");
+		System.out.println("Ingenieria de Software II - Profesor Juan Saldaña");
 
 	}
 
