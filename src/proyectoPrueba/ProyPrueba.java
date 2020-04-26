c34a992b1913acdbcee33c1f1b88e26920555a29
Dayaneth Arrocha <dayamar0409@gmail.com>
2020-04-26 10:42:48 -0500

    Se editó el mensaje

--- src/proyectoPrueba/ProyPrueba.java
+++ src/proyectoPrueba/ProyPrueba.java
@@ -3,7 +3,7 @@ package proyectoPrueba;
 public class ProyPrueba {
 
 	public static void main(String[] args) {
-		System.out.println("Esta en una modificación .... Para que se vea en SmartGit");
+		System.out.println("Practicando editar mensajes");
 
 	}
 
