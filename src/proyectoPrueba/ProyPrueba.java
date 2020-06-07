7f876222cd9f0c0e6f3f9b2f3c66aff516ea1a76
Dayaneth Arrocha <dayamar0409@gmail.com>
2020-06-07 15:45:03 -0500

    Hola

--- src/proyectoPrueba/ProyPrueba.java
+++ src/proyectoPrueba/ProyPrueba.java
@@ -3,7 +3,7 @@ package proyectoPrueba;
 public class ProyPrueba {
 
 	public static void main(String[] args) {
-		System.out.println("Haciendo prueba para TeamCity");
+		System.out.println("Haciendo prueba para TeamCity.");
 
 	}
 
